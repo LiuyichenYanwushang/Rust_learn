5ed470c891f3ac9aa3348ea796331989ee8d30b4
liuyichenYanwushang <3120215754@bit.edu.cn>
2023-02-21 09:13:43 +0800

    add

--- par_try/src/main.rs
+++ par_try/src/main.rs
@@ -4,12 +4,7 @@ use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::Uniform;
 use ndarray_linalg::{Eig};
 fn main() {
-    let a:Array3<f64> = Array::random((100,2000, 2000), Uniform::new(0., 1.));
-    /*
-    for i in 1..100{
-        let (eigvals,eigvecs)=a.slice(s![i,..,..]).eig().unwrap();
-    }
-    */
+    let a:Array3<f64> = Array::random((3,2000, 2000), Uniform::new(0., 1.));
     let results=a.axis_iter(Axis(0)).for_each(|x| {let (eval,evec)=x.eig().unwrap();
     });
 }
