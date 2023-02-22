5311c23d1b5c6de190fbc9e48f27552a853f9bbe
liuyichenYanwushang <3120215754@bit.edu.cn>
2023-02-23 01:34:51 +0800

    add

--- Rustb/src/lib.rs
+++ Rustb/src/lib.rs
@@ -21,7 +21,7 @@ mod Rustb{
         pub atom_list:Vec<usize>,           //模型的原子中的轨道数目, 和原子位置的顺序一致.
         pub ham:Array3::<Complex<f64>>,     //模型的哈密顿量, <m0|H|nR>, 为一个 n_R*nsta*nsta的三维复数张量, 第一个nsta*nsta的矩阵对应的是原胞内的hopping,即 <m0|H|n0>, 后面对应的是 hamR中的hopping.
         pub hamR:Array2::<isize>,           //模型的原胞间haopping的距离, 即 <m0|H|nR> 中的 R
-        pub rmatrix:Array3::<Complex<f64>>  //模型的位置矩阵, 即 <m0|r|nR>.
+        pub rmatrix:Array4::<Complex<f64>>  //模型的位置矩阵, 即 <m0|r|nR>.
     }
     pub fn find_R(hamR:&Array2::<isize>,R:&Array1::<isize>)->bool{
         let n_R:usize=hamR.len_of(Axis(0));
@@ -104,12 +104,12 @@ mod Rustb{
             }
             let mut ham=Array3::<Complex<f64>>::zeros((1,nsta,nsta));
             let mut hamR=Array2::<isize>::zeros((1,dim_r));
-            let mut rmatrix=Array3::<Complex<f64>>::zeros((dim_r,nsta,nsta));
+            let mut rmatrix=Array4::<Complex<f64>>::zeros((1,dim_r,nsta,nsta));
             for i in 1..norb {
                 for r in 1..dim_r{
-                    rmatrix[[r,i,i]]=Complex::<f64>::from(orb[[i,r]]);
+                    rmatrix[[0,r,i,i]]=Complex::<f64>::from(orb[[i,r]]);
                     if spin{
-                        rmatrix[[r,i+norb,i+norb]]=Complex::<f64>::from(orb[[i,r]]);
+                        rmatrix[[0,r,i+norb,i+norb]]=Complex::<f64>::from(orb[[i,r]]);
                     }
                 }
             }
@@ -331,7 +331,7 @@ mod Rustb{
             let mut weights:Vec<usize>=Vec::new();
             let mut n_line:usize=0;
             for i in 3..reads.len(){
-                let string:Vec<usize>=reads[i].trim().split_whitespace().map(|x| x.parse::<usize>().unwrap()).collect();;
+                let string:Vec<usize>=reads[i].trim().split_whitespace().map(|x| x.parse::<usize>().unwrap()).collect();
                 weights.extend(string.clone());
                 println!("{}",reads[i]);
                 if string.len() !=15{
@@ -379,7 +379,7 @@ mod Rustb{
             let mut reads:Vec<String>=Vec::new();
             let mut win_path=file_path.clone();
             win_path.push_str(".win"); //文件的位置
-            let path=Path::new(&xyz_path); //转化为路径格式
+            let path=Path::new(&win_path); //转化为路径格式
             let hr=File::open(path).expect("Unable open the file, please check if have hr file");
             let reader = BufReader::new(hr);
             let mut reads:Vec<String>=Vec::new();
@@ -387,36 +387,84 @@ mod Rustb{
                 let line = line.unwrap();
                 reads.push(line.clone());
             }
-            let read_iter=reads.iter();
+            let mut read_iter=reads.iter();
             let mut lat=Array2::<f64>::zeros((3,3)); //晶格轨道坐标初始化
             let mut spin:bool=false; //体系自旋初始化
             let mut natom:usize=0; //原子位置初始化
             let mut atom=Array2::<f64>::zeros((1,3)); //原子位置坐标初始化
+            let mut proj_name:Vec<&str>=Vec::new();
+            let mut proj_list:Vec<usize>=Vec::new();
+            let mut atom_list:Vec<usize>=Vec::new();
+            let mut atom_name:Vec<&str>=Vec::new();
             loop{
                 let a=read_iter.next();
                 if a==None{
                     break;
                 }else{
-                    if a.includes("begin unit_cell_cart") {
-                        let lat1=read_iter.nest().trim().split_whitespace(); //将数字放到
-                        let lat2=read_iter.nest().trim().split_whitespace();
-                        let lat3=read_iter.nest().trim().split_whitespace();
+                    let a=a.unwrap();
+                    if a.contains("begin unit_cell_cart") {
+                        let mut lat1=read_iter.next().unwrap().trim().split_whitespace(); //将数字放到
+                        let mut lat2=read_iter.next().unwrap().trim().split_whitespace();
+                        let mut lat3=read_iter.next().unwrap().trim().split_whitespace();
                         for i in 0..3{
-                            lat[[0,i]]=lat1.nest().unwrap().parse::<f64>().unwrap();
-                            lat[[1,i]]=lat2.nest().unwrap().parse::<f64>().unwrap();
-                            lat[[2,i]]=lat3.nest().unwrap().parse::<f64>().unwrap();
+                            lat[[0,i]]=lat1.next().unwrap().parse::<f64>().unwrap();
+                            lat[[1,i]]=lat2.next().unwrap().parse::<f64>().unwrap();
+                            lat[[2,i]]=lat3.next().unwrap().parse::<f64>().unwrap();
                         }
-                    } else if a.includes("spinors") && (a.includes("T") || a.includes("t")){
+                    } else if a.contains("spinors") && (a.contains("T") || a.contains("t")){
                         spin=true;
-                    }else if a.includes("begin projections"){
+                    }else if a.contains("begin projections"){
                         loop{
-                            let string=read_iter.nest();
-                            if string.includes("end projections"){
+                            let string=read_iter.next().unwrap();
+                            if string.contains("end projections"){
                                 break
                             }else{ 
-                                let a:Vec<&str>=string.split(|c| c==',' || c==';' || c==':').collect();
+                                let prj:Vec<&str>=string.split(|c| c==',' || c==';' || c==':').collect();
+                                let mut atom_orb_number:usize=0;
+                                for item in prj[1..].iter(){
+                                    println!("{}",*item);
+                                    let aa:usize=match *item{
+                                        "s"=>1,
+                                        "p"=>3,
+                                        "d"=>5,
+                                        "f"=>7,
+                                        "sp3"=>4,
+                                        "sp2"=>3,
+                                        "sp"=>2,
+                                        "sp2d3"=>6,
+                                        "px"=>1,
+                                        "py"=>1,
+                                        "pz"=>1,
+                                        "dxy"=>1,
+                                        "dyz"=>1,
+                                        "dxz"=>1,
+                                        "dz2"=>1,
+                                        "dx2-y2"=>1,
+                                        &_=>panic!("Wrong, no matching"),
+                                    };
+                                    atom_orb_number+=aa;
+                                }
+                                proj_list.push(atom_orb_number);
+                                proj_name.push(prj[0])
                             }
                         }
+                    }else if a.contains("begin atoms_cart"){
+                        loop{
+                            let string=read_iter.next().unwrap();
+                            if string.contains("end atoms_cart"){
+                                break
+                            }else{       
+                                let prj:Vec<&str>=string.split_whitespace().collect();
+                                atom_name.push(prj[0])
+                            }
+                        }
+                    }
+                }
+            }
+            for name in atom_name.iter(){
+                for (j,j_name) in proj_name.iter().enumerate(){
+                    if j_name==name{
+                        atom_list.push(proj_list[j])
                     }
                 }
             }
@@ -432,8 +480,99 @@ mod Rustb{
                 let line = line.unwrap();
                 reads.push(line.clone());
             }
-            let nsta=reads[0].trim().parse::<usize>().unwrap();
+            let nsta=reads[0].trim().parse::<usize>().unwrap()-natom;
             let norb=if spin{nsta/2}else{nsta};
+            let mut orb=Array2::<f64>::zeros((norb,3));
+            let mut atom=Array2::<f64>::zeros((natom,3));
+            for i in 0..norb{
+                let a:Vec<&str>=reads[i+2].trim().split_whitespace().collect();
+                orb[[i,0]]=a[1].parse::<f64>().unwrap();
+                orb[[i,1]]=a[2].parse::<f64>().unwrap();
+                orb[[i,2]]=a[3].parse::<f64>().unwrap();
+            }
+            for i in 0..natom{
+                let a:Vec<&str>=reads[i+2+nsta].trim().split_whitespace().collect();
+                atom[[i,0]]=a[1].parse::<f64>().unwrap();
+                atom[[i,1]]=a[2].parse::<f64>().unwrap();
+                atom[[i,2]]=a[3].parse::<f64>().unwrap();
+            }
+            //开始尝试读取 _r.dat 文件
+
+
+            let mut reads:Vec<String>=Vec::new();
+            let mut r_path=file_path.clone();
+            r_path.push_str("_r.dat");
+            let mut rmatrix=Array4::<Complex<f64>>::zeros((1,3,nsta,nsta));
+            let path=Path::new(&r_path);
+            let hr=File::open(path);
+             
+            if hr.is_ok(){
+                let hr=hr.unwrap();
+                let reader = BufReader::new(hr);
+                let mut reads:Vec<String>=Vec::new();
+                for line in reader.lines() {
+                    let line = line.unwrap();
+                    reads.push(line.clone());
+                }
+                let n_R=reads[2].trim().parse::<usize>().unwrap();
+                for i in 0..n_R{
+                    let mut string=reads[i*nsta*nsta+3].trim().split_whitespace();
+                    let a=string.next().unwrap().parse::<isize>().unwrap();
+                    let b=string.next().unwrap().parse::<isize>().unwrap();
+                    let c=string.next().unwrap().parse::<isize>().unwrap();
+                    if (c>0) || (c==0 && b>0) ||(c==0 && b==0 && a>=0){
+                        println!("a={},b={},c={}",a,b,c);
+                        if a==0 && b==0 && c==0{
+                            for ind_i in 0..nsta{
+                                for ind_j in 0..nsta{
+                                    let mut string=reads[i*nsta*nsta+ind_i*nsta+ind_j+3].trim().split_whitespace();
+                                    string.nth(4);
+                                    for r in 0..3{
+                                        let re=string.next().unwrap().parse::<f64>().unwrap();
+                                        let im=string.next().unwrap().parse::<f64>().unwrap();
+                                        rmatrix[[0,r,ind_i,ind_j]]=Complex::new(re,im);
+                                    }
+                                }
+                            }
+                        }else{
+                            let mut matrix=Array3::<Complex<f64>>::zeros((1,nsta,nsta));
+                            for ind_i in 0..nsta{
+                                for ind_j in 0..nsta{
+                                    let mut string=reads[i*nsta*nsta+ind_i*nsta+ind_j+n_line].trim().split_whitespace();
+                                    let re=string.nth(5).unwrap().parse::<f64>().unwrap();
+                                    let im=string.next().unwrap().parse::<f64>().unwrap();
+                                    matrix[[0,ind_i,ind_j]]=Complex::new(re,im);
+                                }
+                            }
+                            ham.append(Axis(0),matrix.view()).unwrap();
+                        }
+                    }
+                }
+            }else{
+               for i in 1..norb {
+                    for r in 1..3{
+                        rmatrix[[0,r,i,i]]=Complex::<f64>::from(orb[[i,r]]);
+                        if spin{
+                            rmatrix[[0,r,i+norb,i+norb]]=Complex::<f64>::from(orb[[i,r]]);
+                        }
+                    }
+                }
+            }
+            let mut model=Model{
+                dim_r:3,
+                norb,
+                nsta,
+                natom,
+                spin,
+                lat,
+                orb,
+                atom,
+                atom_list,
+                ham,
+                hamR,
+                rmatrix,
+            };
+            model
         }
     }
 }
--- read_hr/src/main.rs
+++ read_hr/src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 use nalgebra::Complex;
 use ndarray::*;
 fn main() {
-    from_hr("/home/liuyichen/Rust_study/read_hr/","wannier90");
+    from_hr("./","wannier90");
 }
 pub fn from_hr(path:&str,file_name:&str){
 //我们首先读 hr.dat 文件
@@ -55,7 +55,6 @@ pub fn from_hr(path:&str,file_name:&str){
         let b=string.next().unwrap().parse::<isize>().unwrap();
         let c=string.next().unwrap().parse::<isize>().unwrap();
         if (c>0) || (c==0 && b>0) ||(c==0 && b==0 && a>=0){
-            println!("a={},b={},c={}",a,b,c);
             if a==0 && b==0 && c==0{
                 hamR[[0,0]]=a;
                 hamR[[0,1]]=b;
@@ -83,9 +82,10 @@ pub fn from_hr(path:&str,file_name:&str){
             }
         }
     }
+    /*
     //文件读取完成
     println!("{:?}",hamR);
-    for i in 0..hamR.len_of(Axis(0)){
+    for i in 1..hamR.len_of(Axis(0)){
         let R=-hamR.slice(s![i,..]).to_owned();
         let have=find_R(&hamR,&R);
         if have {
@@ -93,6 +93,135 @@ pub fn from_hr(path:&str,file_name:&str){
             println!("{:?},{:?}",R,hamR.slice(s![i,..]).to_owned())
         }
     }
+    */
+    //开始读取 .win 文件
+    let mut reads:Vec<String>=Vec::new();
+    let mut win_path=file_path.clone();
+    win_path.push_str(".win"); //文件的位置
+    let path=Path::new(&win_path); //转化为路径格式
+    let hr=File::open(path).expect("Unable open the file, please check if have hr file");
+    let reader = BufReader::new(hr);
+    let mut reads:Vec<String>=Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        reads.push(line.clone());
+    }
+    let mut read_iter=reads.iter();
+    let mut lat=Array2::<f64>::zeros((3,3)); //晶格轨道坐标初始化
+    let mut spin:bool=false; //体系自旋初始化
+    let mut natom:usize=0; //原子位置初始化
+    let mut atom=Array2::<f64>::zeros((1,3)); //原子位置坐标初始化
+    let mut proj_name:Vec<&str>=Vec::new();
+    let mut proj_list:Vec<usize>=Vec::new();
+    let mut atom_list:Vec<usize>=Vec::new();
+    let mut atom_name:Vec<&str>=Vec::new();
+    loop{
+        let a=read_iter.next();
+        if a==None{
+            break;
+        }else{
+            let a=a.unwrap();
+            if a.contains("begin unit_cell_cart") {
+                let mut lat1=read_iter.next().unwrap().trim().split_whitespace(); //将数字放到
+                let mut lat2=read_iter.next().unwrap().trim().split_whitespace();
+                let mut lat3=read_iter.next().unwrap().trim().split_whitespace();
+                for i in 0..3{
+                    lat[[0,i]]=lat1.next().unwrap().parse::<f64>().unwrap();
+                    lat[[1,i]]=lat2.next().unwrap().parse::<f64>().unwrap();
+                    lat[[2,i]]=lat3.next().unwrap().parse::<f64>().unwrap();
+                }
+            } else if a.contains("spinors") && (a.contains("T") || a.contains("t")){
+                spin=true;
+            }else if a.contains("begin projections"){
+                loop{
+                    let string=read_iter.next().unwrap();
+                    if string.contains("end projections"){
+                        break
+                    }else{ 
+                        let prj:Vec<&str>=string.split(|c| c==',' || c==';' || c==':').collect();
+                        let mut atom_orb_number:usize=0;
+                        for item in prj[1..].iter(){
+                            println!("{}",*item);
+                            let aa:usize=match *item{
+                                "s"=>1,
+                                "p"=>3,
+                                "d"=>5,
+                                "f"=>7,
+                                "sp3"=>4,
+                                "sp2"=>3,
+                                "sp"=>2,
+                                "sp2d3"=>6,
+                                "px"=>1,
+                                "py"=>1,
+                                "pz"=>1,
+                                "dxy"=>1,
+                                "dyz"=>1,
+                                "dxz"=>1,
+                                "dz2"=>1,
+                                "dx2-y2"=>1,
+                                &_=>panic!("Wrong, no matching"),
+                            };
+                            atom_orb_number+=aa;
+                        }
+                        proj_list.push(atom_orb_number);
+                        proj_name.push(prj[0])
+                    }
+                }
+            }else if a.contains("begin atoms_cart"){
+                loop{
+                    let string=read_iter.next().unwrap();
+                    if string.contains("end atoms_cart"){
+                        break
+                    }else{       
+                        let prj:Vec<&str>=string.split_whitespace().collect();
+                        atom_name.push(prj[0])
+                    }
+                }
+            }
+        }
+    }
+    for name in atom_name.iter(){
+        for (j,j_name) in proj_name.iter().enumerate(){
+            if j_name==name{
+                atom_list.push(proj_list[j])
+            }
+        }
+    }
+    natom=atom_list.len();
+    println!("{:?}",atom_name);
+    println!("{:?}",atom_list);
+    println!("{}",atom_list.len());
+    println!("{:?}",proj_list);
+    println!("{:?}",proj_name);
+    //开始读取 seedname_centres.xyz 文件
+    let mut reads:Vec<String>=Vec::new();
+    let mut xyz_path=file_path.clone();
+    xyz_path.push_str("_centres.xyz");
+    let path=Path::new(&xyz_path);
+    let hr=File::open(path).expect("Unable open the file, please check if have hr file");
+    let reader = BufReader::new(hr);
+    let mut reads:Vec<String>=Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        reads.push(line.clone());
+    }
+    let nsta=reads[0].trim().parse::<usize>().unwrap()-natom;
+    let norb=if spin{nsta/2}else{nsta};
+    let mut orb=Array2::<f64>::zeros((norb,3));
+    let mut atom=Array2::<f64>::zeros((natom,3));
+    for i in 0..norb{
+        let a:Vec<&str>=reads[i+2].trim().split_whitespace().collect();
+        orb[[i,0]]=a[1].parse::<f64>().unwrap();
+        orb[[i,1]]=a[2].parse::<f64>().unwrap();
+        orb[[i,2]]=a[3].parse::<f64>().unwrap();
+    }
+    for i in 0..natom{
+        let a:Vec<&str>=reads[i+2+nsta].trim().split_whitespace().collect();
+        atom[[i,0]]=a[1].parse::<f64>().unwrap();
+        atom[[i,1]]=a[2].parse::<f64>().unwrap();
+        atom[[i,2]]=a[3].parse::<f64>().unwrap();
+    }
+    println!("{:?}",atom)
 }
 fn find_R(hamR:&Array2::<isize>,R:&Array1::<isize>)->bool{
     let n_R:usize=hamR.len_of(Axis(0));
